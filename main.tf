4681ce07155f38af38652aa87ce80d280c4ede3f
Gustavo Inacio <gustavo.inacio@getmore.com.br>
2021-01-25 15:10:28 -0300

    Initial commit

---
+++ main.tf
@@ -0,0 +1,58 @@
+
+
+resource "aws_ecr_repository" "this" {
+  name = var.name
+  tags = {
+    Project = var.project
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "this" {
+  repository = aws_ecr_repository.this.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Expire untagged images older than 14 days",
+            "selection": {
+                "tagStatus": "untagged",
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": 14
+            },
+            "action": {
+                "type": "expire"
+            }
+        },
+        {
+            "rulePriority": 2,
+            "description": "Keep last 5 prod images",
+            "selection": {
+                "tagStatus": "tagged",
+                "tagPrefixList": ["prod", "production"],
+                "countType": "imageCountMoreThan",
+                "countNumber": 5
+            },
+            "action": {
+                "type": "expire"
+            }
+        },
+        {
+            "rulePriority": 3,
+            "description": "Keep last 3 dev images",
+            "selection": {
+                "tagStatus": "tagged",
+                "tagPrefixList": ["dev", "sandbox", "development"],
+                "countType": "imageCountMoreThan",
+                "countNumber": 3
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
